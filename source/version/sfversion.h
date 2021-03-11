77f00719f07f7f8b360f6a2ef7f092ad8f8d8256
David <david@scopefun.com>
2021-03-11 18:12:10 +0100

    beta

--- source/version/sfversion.h
+++ source/version/sfversion.h
@@ -22,5 +22,5 @@
 #define EXE_VERSION_MAJOR       "2"
 #define EXE_VERSION_MINOR       "1"
 #define EXE_VERSION_MICRO       "0"
-#define EXE_VERSION_TIMESTAMP   "15:34 06-02-2021"
-#define EXE_VERSION_TYPE        "release"
+#define EXE_VERSION_TIMESTAMP   "18:08 11-03-2021"
+#define EXE_VERSION_TYPE        "beta release"
