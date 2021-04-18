460d07cb1aa933d996d0050fe3ce2f89a1a306f8
David <david@scopefun.com>
2021-04-18 17:03:10 +0200

    version 2.1.1

--- source/version/sfversion.h
+++ source/version/sfversion.h
@@ -21,6 +21,6 @@
 
 #define EXE_VERSION_MAJOR       "2"
 #define EXE_VERSION_MINOR       "1"
-#define EXE_VERSION_MICRO       "0"
+#define EXE_VERSION_MICRO       "1"
 #define EXE_VERSION_TIMESTAMP   "18:08 11-03-2021"
 #define EXE_VERSION_TYPE        "release"
